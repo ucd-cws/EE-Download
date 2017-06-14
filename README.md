58f0a85b8080329a3b48ad346c5d3dda5e3843d2
Nick <nickrsan@users.noreply.github.com>
2017-06-13 21:36:49 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Earth Engine Downloader
+This code downloads all images in an image collection from Earth Engine to a specified folder. It can be called from the command line or a Python module.
+
+## Installation
+This library requires you have set up access to Earth Engine via its Python API and already authenticated your account. [See here for more information](https://developers.google.com/earth-engine/python_install) on how to do that.
+If you're on Windows, I recommend using Bash on Windows for this project, but won't detail that further here - the installation instructions below assume a bash prompt.
+
+### Dependencies
+The only dependency for this script, besides the Earth Engine Python API is pycurl for downloading the images. pycurl requires some system setup.
+
+```
+sudo apt-get install libcurl-dev
+sudo pip install pycurl
+```
+
+You have to install the development package for curl before pycurl will compile. The above commands should handle installation.
+
+## Command line usage
+If you call the script on the command line, it has three simple, positional parameters:
+
+1. The path or identifier of the ImageCollection to downloading
+2. The output folder on the local machine to save images into
+3. The maximum number of images to download
+
+```
+	python ee_download.py "users/yourusername/path/to/image/collection" "/mnt/c/myusername/path/to/outputs" 9999
+```
+
+## Usage in Python
+After installation, you can import the module and call the main functions - parameters to the bulk downloader are the same
+as the command line usage above
+
+```
+	import ee_download
+	downloaded_images = ee_download.download_images_in_collection(collection_id = "users/yourusername/path/to/image/collection",
+																	output_folder = "/mnt/c/myusername/path/to/outputs",
+																	max_items = 9999
+																)
+```
+
+Alternatively, if you just want to use the single image downloading mechanism, you can run
+```
+	downloaded_images = ee_download.download_image_by_asset_path("users/yourusername/some/single/image",
+																	output_folder = "/mnt/c/myusername/path/to/outputs",
+																)
+```
+
+Each function returns the full paths to the downloaded items as a list.
---
+++ ee_download.py
@@ -0,0 +1,102 @@
+import sys
+import os
+import errno
+from zipfile import is_zipfile, ZipFile
+
+import pycurl
+
+import ee
+import ee.mapclient
+
+max_items = 99999
+
+ee.Initialize()
+
+def download_image_by_asset_path(asset_path, output_folder, overwrite=True):
+	"""
+		Downloads an individual image, given its asset path, and saves it to output_folder.
+		Returns a list of the downloaded image or images
+	"""
+	### Get the download URL from EE
+	image = ee.Image(asset_path)  # this approach comes from https://github.com/google/earthengine-api/blob/master/python/examples/py/Image/download.py
+	path = image.getDownloadUrl({
+		#'scale': 30,
+		#'crs': 'EPSG:3310',
+		#'region': '[[-120, 35], [-119, 35], [-119, 34], [-120, 34]]'
+	})
+	
+	### Do some name management things
+	output_name = os.path.split(asset_path)[1]
+	zipfile = output_name + ".zip"
+	download_path = os.path.join(output_folder, zipfile)  # comes as a zip file with a .tfw
+	output_path = os.path.join(output_folder, output_name + ".tif")
+	
+	### Download the file
+	with open(download_path, 'wb') as f:
+		c = pycurl.Curl()
+		c.setopt(c.URL, path)
+		c.setopt(c.WRITEDATA, f)
+		c.perform()
+		c.close()
+		
+	### Extract the Zip and delete it
+	if not is_zipfile(download_path):
+		raise RuntimeError("Downloaded file was not a zip file!")
+	
+	with open(download_path, 'r') as zf:
+		z = ZipFile(zf)
+		downloaded_items = [os.path.join(output_folder,item) for item in z.namelist() if not item.endswith("tfw") ]
+		z.extractall(path=output_folder)
+		z.close()
+		del z
+	
+	os.remove(download_path)
+	
+	return downloaded_items
+
+	
+def makedirs(path):
+	"""
+		Handles nice directory creation in Python 2.7 - see https://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python/600612#600612
+	"""
+	try:
+		os.makedirs(path)
+	except OSError as exc:  # Python >2.5
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else:
+			raise
+
+	
+def download_images_in_collection(collection_id, output_folder, max_items=max_items):
+	"""
+		Downloads images in ImageCollection specified by collection_id and saves them into 
+		the location specified in output folder, up to max_items downloads. Set max_items
+		to a very high number to download all items.
+		
+		Returns a list with the full paths to the downloaded images.
+	"""
+	
+	### Get all of the items in the collection
+	collection = ee.ImageCollection(collection_id)
+	collection_items = collection.toList(max_items).getInfo()
+	
+	# check that the output path exists - create it if not
+	makedirs(output_folder)
+	
+	downloaded_items = []
+	for item in collection_items:
+		downloaded_items += download_image_by_asset_path(item["id"], output_folder)  # extend the list with the new list that's produced - don't append
+		
+	return downloaded_items
+
+if __name__ == "__main__":
+	args = sys.argv[1:]
+	collection_id = args[0]
+	output_folder = args[1]
+	max_items = int(args[2])
+
+	collection_id = "users/ucd-cws-ee-data/ssj-delta-cu/ssj-disalexi/eto_collection"  # for testing
+	output_folder = "/mnt/c/Users/dsx/Code/ee-download/test"
+	
+	download__images_in_collection(collection_id, output_folder, max_items=max_items)
